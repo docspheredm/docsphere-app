f046baffb25977137fc03b5b298e3a9cba0429a1
Docsphere <docspheredm@gmail.com>
2026-01-31 15:11:09 +0530

    Fix React hydration mismatch in RootLayout - suppress warnings on head element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
+    <html lang="en" suppressHydrationWarning={true}>
+      <head suppressHydrationWarning={true}>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
